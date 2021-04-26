f441b6db4889dddd6fa1adbafbec0ce41314c32c
Brian Musembi <brian.musembi@mths.ca>
2021-04-26 14:40:37 +0000

    edited area_and_perimeter.cpp

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 26 April 2021
+// This program calculates the area of a rectangle 73m by 82m
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "If a circle has the radius of:" << std::endl;
+  std::cout << "15 mm" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Its area is " << (M_PI * pow(15, 2)) << " mm²." << std::endl;
+  std::cout << std::endl;
+  std::cout << "And" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Its perimeter is " << (2 * M_PI * 15) << " mm." << std::endl;}
